1b0cb22d93c48db1b29cdeb5f90bd539c0aef140
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-09-21 16:26:02 -0500

    nuevas cosas

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,7 +1,28 @@
+import style from './projects.module.css'
+import image from '../../icons/icons8-reaccionar-120.png'
+
 export default function Projects(){
     return (
-        <h1>
-            Soy el Projects
-        </h1>
+        <section className={style.containerMax}>
+            <h1>Proyectos</h1>
+            <div className={style.projectContainer}>
+                <img src={image.src} className={style.image}/>
+                <h1 className={style.titleProject}>Ejemplo projects</h1>
+                <p className={style.p}>FechaDelProject</p>
+                <p className={style.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus in aliquam earum perferendis maiores nihil dignissimos maxime iure aut veniam reiciendis, velit hic itaque natus? Molestias ipsam alias architecto recusandae?</p>
+            </div>
+            <div className={style.projectContainer}>
+                <img src={image.src} className={style.image}/>
+                <h1 className={style.titleProject}>Ejemplo projects</h1>
+                <p className={style.p}>FechaDelProject</p>
+                <p className={style.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus in aliquam earum perferendis maiores nihil dignissimos maxime iure aut veniam reiciendis, velit hic itaque natus? Molestias ipsam alias architecto recusandae?</p>
+            </div>
+            <div className={style.projectContainer}>
+                <img src={image.src} className={style.image}/>
+                <h1 className={style.titleProject}>Ejemplo projects</h1>
+                <p className={style.p}>FechaDelProject</p>
+                <p className={style.p}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus in aliquam earum perferendis maiores nihil dignissimos maxime iure aut veniam reiciendis, velit hic itaque natus? Molestias ipsam alias architecto recusandae?</p>
+            </div>
+        </section>
     )
 }
