768f5fae2b9a190ba295cc5ec106e3b741135e82
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-09-24 14:12:44 -0500

    cositas

--- app/contacto/page.tsx
+++ app/contacto/page.tsx
@@ -1,7 +1,22 @@
+import style from './contacto.module.css'
+import me from '../../icons/45999__1_-removebg-preview.png'
+
 export default function Contacto () {
     return(
-        <h1>
-            This is a Contact page
-        </h1>
+        <section className={style.containerMax}>
+            <div className={style.left}>
+                <div className={style.div1}>
+                <h1>Email</h1>
+                <h1 className={style.title}>marcos2679525@gmail.com</h1>
+                </div>
+                <div>
+                <h1>Numero</h1>
+                <h1 className={style.title}>+57 3007479326</h1>
+                </div>
+            </div>
+            <div className={style.right}>
+                <img src={me.src} className={style.me}/>
+            </div>
+        </section>
     )
 }
