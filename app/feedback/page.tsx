65c9081b49918afb27b8127209caee738df4e0e0
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-09-23 23:46:36 -0500

    cositas

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -18,12 +18,12 @@ export default function Feedback(){
             <div className={style.feedbackContainer}>
                 <h1>Comentarios</h1>
                 <div className={style.feedback}>
-                    <h1>Julian montez</h1>
+                    <h1  className={style.title}>Julian montez</h1>
                     <p>Fecha de comentario</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tenetur cum dolor, provident ea odio suscipit itaque quam labore quas tempora ipsum impedit possimus animi odit consectetur a eaque repudiandae.</p>
                 </div>
                 <div className={style.feedback}>
-                    <h1>Julian montez</h1>
+                    <h1 className={style.title}>Julian montez</h1>
                     <p>Fecha de comentario</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tenetur cum dolor, provident ea odio suscipit itaque quam labore quas tempora ipsum impedit possimus animi odit consectetur a eaque repudiandae.</p>
                 </div>
