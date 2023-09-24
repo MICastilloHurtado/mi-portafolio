1b6fdeef16a9c78f57534d3270601e40c53f9b85
MICastilloHurtado <122422130+MICastilloHurtado@users.noreply.github.com>
2023-09-23 22:38:11 -0500

    construiccion de feedback!

--- app/feedback/page.tsx
+++ app/feedback/page.tsx
@@ -1,7 +1,33 @@
+import style from './feedBack.module.css'
+
 export default function Feedback(){
     return(
-        <div>
-            <h1 className="feedback">Feedback hecho ya</h1><br/>
-        </div>
+        <section className={style.containerMax}>
+            <h1>
+            Deja tu feedBack y dime que piensas que puedo mejorar o aprender para ser mejor  🫶 
+            </h1>
+            <div className={style.inputContainer}>
+                <span>Nombre</span>
+                <input/>
+            </div>
+            <div className={style.inputContainer}>
+                <span>texto</span>
+                <input/>
+            </div>
+            <button>Enviar</button>
+            <div className={style.feedbackContainer}>
+                <h1>Comentarios</h1>
+                <div className={style.feedback}>
+                    <h1>Julian montez</h1>
+                    <p>Fecha de comentario</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tenetur cum dolor, provident ea odio suscipit itaque quam labore quas tempora ipsum impedit possimus animi odit consectetur a eaque repudiandae.</p>
+                </div>
+                <div className={style.feedback}>
+                    <h1>Julian montez</h1>
+                    <p>Fecha de comentario</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio tenetur cum dolor, provident ea odio suscipit itaque quam labore quas tempora ipsum impedit possimus animi odit consectetur a eaque repudiandae.</p>
+                </div>
+            </div>
+        </section>
     )
 }
